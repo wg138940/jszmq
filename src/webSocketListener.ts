79ecc6cc86e1863a58c29c8e816c591d08f5163e
WG <wg138940@outlook.com>
2023-10-04 01:24:02 +0800

    * fix: 'upgrade' handled by HttpServerListener

--- src/webSocketListener.ts
+++ src/webSocketListener.ts
@@ -1,4 +1,4 @@
-import ws from 'websocket';
+import ws from 'websocket'
 import * as url from 'url'
 import {toNumber} from 'lodash-es'
 import { EventEmitter } from 'events'
@@ -6,6 +6,7 @@ import SocketOptions from './socketOptions.js'
 import Endpoint from './webSocketEndpoint.js'
 import * as http from 'http'
 import * as https from 'https'
+import * as net from "net"
 import {IListener} from './types.js'
 
 type HttpServer = http.Server | https.Server
@@ -14,14 +15,41 @@ class HttpServerListener {
     servers = new Map<string, ws.server>()
 
     constructor(private server:HttpServer) {
+        server.on('upgrade', this.onUpgrade.bind(this))
+        this.onWsRequest = this.onWsRequest.bind(this)
+    }
 
+    onWsRequest(request: ws.request) {
+        if (request.requestedProtocols.includes('zws2.0'))
+            request.accept('zws2.0', request.origin)
+        else
+            request.reject(400)
+    }
+
+    onUpgrade(request:http.IncomingMessage, socket: net.Socket) {
+        if (request.url) {
+            const path = url.parse(request.url).pathname
+
+            if (path) {
+                const wsServer = this.servers.get(path)
+
+                if (wsServer) {
+                    // ws.server emits 'request' event after handleUpgrade()
+                    wsServer.handleUpgrade(request, socket)
+                    return
+                }
+            }
+        }
+        socket.destroy()
     }
 
     add(path:string, wsServer: ws.server) {
+        wsServer.on('request', this.onWsRequest)
         this.servers.set(path, wsServer)
     }
 
     remove(path:string) {
+        this.servers.get(path)?.off('request', this.onWsRequest)
         this.servers.delete(path)
 
         if (this.servers.size === 0)
@@ -78,12 +106,17 @@ export default class WebSocketListener extends EventEmitter implements IListener
             this.ownHttpServer = httpServer
         }
         
+        // The code is a hack to ensure ws.server is configured without interfering with 'upgrade' event. 
+        // ws.server.mount() is not supposed to be called as it adds an 'upgrade' listener disrupting HttpServerListener logic.
+        // However, without calling ws.server.mount(), ws.server isn't automatically configured and cannot handle requests properly.
         this.server = new ws.server()
+        this.server.mount({ httpServer })
+        this.server.unmount()
+
         const listener = getHttpServerListener(httpServer)
         this.path = addrUrl.pathname!
         listener.add(addrUrl.pathname!, this.server)
         this.server.on('connect', this.onConnection)
-        this.server.mount({ httpServer, autoAcceptConnections: true })
 
         if (this.ownHttpServer) {
             this.ownHttpServer.listen(port, addrUrl.hostname!)
